6efa7d2864b00314717e636021a19bc662daa747
Toan Le Viet <toan.le2@tiki.vn>
2019-04-14 01:01:11 +0700

    update code

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const feathers = require('@feathersjs/feathers');
+const express = require('@feathersjs/express');
+const Facebook = require('./routers/Facebook');
 
-const app = feathers();
+const PORT = process.env.PORT || 3000;
 
-app.use('todos', {
-  async get(name) {
-    return {
-      name,
-      text: `Hello ${name}`,
-    };
-  },
-});
+// This creates an app that is both, an Express and Feathers app
+const app = express(feathers());
 
-async function getTodo(name) {
-  const service = app.service('todos');
-  const todos = await service.get(name);
-  console.log(todos);
-}
+// Turn on JSON body parsing for REST services
+app.use(express.json());
+// Turn on URL-encoded body parsing for REST services
+app.use(express.urlencoded({ extended: true }));
+// Set up REST transport using Express
+app.configure(express.rest());
 
-getTodo('Toan');
+// Set up an error handler that gives us nicer errors
+app.use(express.errorHandler());
+
+app.use('/facebook', Facebook);
+
+// app.get('/facebook/get-random-token', () => {
+//   app.service('facebook').getAccessToken();
+// });
+
+// Start the server on port
+app.listen(PORT);
---
+++ routers/Facebook.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const Facebook = require('../services/Facebook');
+
+const router = express.Router();
+const facebook = new Facebook();
+
+router.get('/get-random-token', async (req, res) => {
+  const response = await facebook.getAccessToken();
+  res.send(response);
+});
+
+module.exports = router;
